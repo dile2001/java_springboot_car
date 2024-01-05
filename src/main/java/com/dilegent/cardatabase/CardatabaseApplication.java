8c19b0183af91a9011548f9b0d588da3a4bd55cc
DILEGENT1 <di.abaki@gmail.com>
2024-01-05 22:32:00 +0400

    feat: secure with user table

--- src/main/java/com/dilegent/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/dilegent/cardatabase/CardatabaseApplication.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.dilegent.cardatabase.domain.AppUser;
+import com.dilegent.cardatabase.domain.AppUserRepository;
 import com.dilegent.cardatabase.domain.Car;
 import com.dilegent.cardatabase.domain.CarRepository;
 import com.dilegent.cardatabase.domain.Owner;
@@ -20,9 +23,11 @@ public class CardatabaseApplication implements CommandLineRunner {
 	);
 	private final CarRepository repository;
 	private final OwnerRepository orepository;
-	public CardatabaseApplication(CarRepository repository, OwnerRepository orepository) {
+	private final AppUserRepository urepository;
+	public CardatabaseApplication(CarRepository repository, OwnerRepository orepository, AppUserRepository urepository) {
 		this.repository = repository;
 		this.orepository = orepository;
+		this.urepository = urepository;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
@@ -46,6 +51,10 @@ public class CardatabaseApplication implements CommandLineRunner {
 		      logger.info("brand: {}, model: {}",
 		          car.getBrand(), car.getModel());
 		  }
+		  // Username: user, password: user
+        urepository.save(new AppUser("user", "$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue","USER"));
+        // Username: admin, password: admin
+        urepository.save(new AppUser("admin", "$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW", "ADMIN"));
 		
 	}
 
--- src/main/java/com/dilegent/cardatabase/SecurityConfig.java
+++ src/main/java/com/dilegent/cardatabase/SecurityConfig.java
@@ -1,15 +1,38 @@
 package com.dilegent.cardatabase;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.
-  EnableWebSecurity;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
-/*
- * The @Configuration and @EnableWebSecurity annotations 
- * switch off the default web security configuration, 
- * and we can define our own configuration in this class.
- */
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.dilegent.cardatabase.service.UserDetailsServiceImpl;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	private final UserDetailsServiceImpl userDetailsService;
+	public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+	    this.userDetailsService = userDetailsService;
+	}
+	//	Delete this method to disable in-memory users.
+	// @Bean
+	// public InMemoryUserDetailsManager userDetailsService() {
+	//     UserDetails user = User.builder().username("user").
+	//         password(passwordEncoder().encode("password"))
+	//         .roles("USER").build();
+	//     return new InMemoryUserDetailsManager(user);
+	// }
+		
+	// Add a new configureGlobal method to enable users from the database.
+
+	public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception
+	{
+	  auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}	
+	 @Bean
+	public PasswordEncoder passwordEncoder() {
+	     return new BCryptPasswordEncoder();
+	}
 }
